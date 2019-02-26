bb5d07a57250a94be3bfa6e6ae22212475bbdc4d
neliza <nicolasaelf64@gmail.com>
2019-02-25 20:55:59 -0700

    completed results string function

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ document.addEventListener('DOMContentLoaded', function(){
     
     //Calculate function that takes three parameters(firstNumber, operator, secondNumber)
     const calculate = (n1, operator, n2) => {
-        //let result = '';
-
         const firstInput = parseFloat(n1);
         const secondInput = parseFloat(n2);
 
@@ -19,62 +17,58 @@ document.addEventListener('DOMContentLoaded', function(){
         if(operator === 'multiply')
             return firstInput * secondInput
         if(operator === 'divide')
-            return firstInput / secondInput
-
-        //if (operator === 'add'){
-            //result = parseFloat(n1) + parseFloat(n2)
-        //} else if (operator === 'subtract'){
-           //result = parseFloat(n1) - parseFloat(n2)
-        //} else if (operator === 'multiply'){
-            //result = parseFloat(n1) * parseFloat(n2)
-        //} else if (operator === 'divide'){
-            //result = parseFloat(n1) / parseFloat(n2)
-        //}                
-        //return result;
+            return firstInput / secondInput        
     }
     
-    //We need to listen for when a button is clicked
     keys.addEventListener('click', e => {
-        if (e.target.matches('button')){                        
-            //Here we are getting the value of the key pressed
-            //Either it is an action key or a number key
-            const key = e.target;
-            
+        if (e.target.matches('button')){  
+            const key = e.target;            
             //Here we are getting the action of the button set in the data-action of the HTML
             const action = key.dataset.action;
-
             //The number of the button that is pressed 
             const keyTextContent = key.textContent;
-
             //Let's get the display number from HTML
             const displayedNumber = display.textContent;
             //console.log(displayedNumber) //This is printing 0 at this point 
-
             //Setting custom attribute 'previous key type' to a variable
             const previousKeyType = calculator.dataset.previousKeyType;
+            //State of the calculator
+            const state = calculator.dataset
 
-            //Remove selected class 
-                //Remove the selected class from all keys with for loop
-                //In the console, key is: <button data-action="subtract" class="selected">...</button>
-                //Use Array.from(), since the keys are not really an array. We are creating an array
-                //The parent here is calculator_keys and all the children are the buttons
+            const getKeyType = (key) => {
+                const {action} = key.dataset
+                if(!action) return 'number'
+                if(
+                    action === 'add' ||
+                    action === 'subtract' ||
+                    action === 'multiply' ||
+                    action === 'divide'
+                ) return 'operator'
+                return action
+            }
 
             //Create Results String Function
-            const createResultsString = () => {
+            const createResultsString = (key, displayedNumber, state) => {
+                const keyType = getKeyType(key)
+
                 //Variables Needed:
                 //keyTextContent
+                const keyTextContent = key.textContent
                 //displayedNum
+                
                 //previousKeyType
+                const previousKeyType = state.previousKeyType                
                 //action
-                //firstNumber
-                //secondNumber
-                //operator
+                const action = key.dataset.action                
+                //calculator.dataset.firstValue = firstNumber  
+                const firstNumber = state.firstNumber             
                 //calculator.dataset.previousKeyType = 'operator' -- setting custom attribute
-                //calculator.dataset.firstValue = displayedNumber;
-
+                const operator = state.operator
+                //calculator.dataset.firstValue = modifiedValue
+                const modifiedValue = state.modifiedValue
 
                 //This is for Numbers, as there is no action here
-                if(!action) {
+                if(keyType === 'number') {
                    return 
                         displayedNumber === '0' || 
                         previousKeyType === 'operator' || 
@@ -83,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     : displayedNumber + keyTextContent                    
                 }  
                 
-                if(action === 'decimal'){
+                if(keyType === 'number'){
                     if(!displayedNumber.includes('.'))
                         return displayedNumber + '.'
                     if (previousKeyType === 'operator' || previousKeyType === 'calculate')
@@ -93,16 +87,11 @@ document.addEventListener('DOMContentLoaded', function(){
                     return displayedNumber                    
                 }
 
-                if(
-                    action === 'add' ||
-                    action === 'subtract' ||
-                    action === 'multiply' ||
-                    action === 'divide'
-                ){
+                if(keyType === 'operator')
+                {
                     const firstNumber = calculator.dataset.firstValue;
                     const operator = calculator.dataset.operation;
-                    //const secondNumber = displayedNumber;
-
+                    
                     return 
                         firstNumber && 
                         operator && 
@@ -113,8 +102,16 @@ document.addEventListener('DOMContentLoaded', function(){
                     : displayedNumber                    
                 }
 
-                if(action === 'clear') 
+                if(keyType === 'clear') 
                     return 0
+
+                if(keyType === 'calculate'){
+                    if (firstNumber){
+                        return previousKeyType === 'calculate'
+                            ? calculate(firstNumber, operator, modifiedValue)        
+                            : calculate(firstNumber, operator, displayedNumber)
+                    }
+                }
             }
            
             //Release selected state
@@ -139,48 +136,48 @@ document.addEventListener('DOMContentLoaded', function(){
 
                 //We only need to check for the first number and the operator to calculate for consecutive calculations
                 //If the previous key is an operator, then we do not call the calculate function
-                if (firstNumber && operator && previousKeyType !== 'operator' && previousKeyType === 'calculate') {
+                //if (firstNumber && operator && previousKeyType !== 'operator' && previousKeyType === 'calculate') {
 
-                    const calculatedValue = calculate(firstNumber, operator, secondNumber);
-                    display.textContent = calculatedValue;
+                    //const calculatedValue = calculate(firstNumber, operator, secondNumber);
+                   // display.textContent = calculatedValue;
 
                     //STUDY THIS!!
                     //firstNumber will now equal the updated calculated value; updating the custom data attribute firstNumber
-                    calculator.dataset.firstNumber = calculatedValue;                    
-                } else {
+                    //calculator.dataset.firstNumber = calculatedValue;                    
+               //} else {
                     //If no additional calculations need to take place the first number entered to the currently displayed number
-                    calculator.dataset.firstNumber = displayedNumber;
-                }
+                    //calculator.dataset.firstNumber = displayedNumber;
+                //}
 
                 //I want to add a class to show the user that they have clicked an operator key
-                key.classList.add("selected");   
+                //key.classList.add("selected");   
                 
                 //We need to do two things:
                 //1) store first value entered in custom attribute
                 //2) store the operator entered in custom attribute
 
                 //calculator.dataset.firstValue = displayedNumber;
-                calculator.dataset.operation = action;                
-            }
+                //calculator.dataset.operation = action;                
+            //}
             
-            if (action === 'calculate'){
-                console.log(action)
+            //if (action === 'calculate'){
+                //console.log(action)
                 //We will get the second value entered here and save it in a variable
-                let secondNumber = displayedNumber
+                //let secondNumber = displayedNumber
                 
                 //Repeat for first number entered
-                let firstNumber = calculator.dataset.firstValue
+               // let firstNumber = calculator.dataset.firstValue
 
                 //Repeat for operator
-                const operator = calculator.dataset.operation;
+                //const operator = calculator.dataset.operation;
 
-                if (firstNumber){
-                    if (previousKeyType === 'calculate'){
-                        firstNumber = displayedNumber
-                        secondNumber = calculator.dataset.carriedSecondNumber;
-                    }                
-                display.textContent = calculate(firstNumber, operator, secondNumber);
-                }
+                //if (firstNumber){
+                    //if (previousKeyType === 'calculate'){
+                        //firstNumber = displayedNumber
+                        //secondNumber = calculator.dataset.carriedSecondNumber;
+                    //}                
+                //display.textContent = calculate(firstNumber, operator, secondNumber);
+                //}
 
                 //if (firstNumber && operator && previousKeyType !== 'calculate') {
                     //display.textContent = calculate(firstNumber, operator, secondNumber);
@@ -188,37 +185,37 @@ document.addEventListener('DOMContentLoaded', function(){
                 
                 //We need to carry the second value over into a custom attribute
                 //Set custom attribute
-                calculator.dataset.carriedSecondNumber = secondNumber;
+                //calculator.dataset.carriedSecondNumber = secondNumber;
 
                 //Set custom attribute
-                calculator.dataset.previousKeyType = 'calculate';
-            };
+                //calculator.dataset.previousKeyType = 'calculate';
+            //};
                         
-            if(action === 'clear'){
-                console.log(action);
+            //if(action === 'clear'){
+                //console.log(action);
 
                 //When selecting AC, we need to clear all custom attributes
                 //firstNumber, operator, second number, prevKey type
-                if (key.textContent === 'AC'){
-                    calculator.dataset.firstNumber = ''
-                    calculator.dataset.operation = ''
-                    calculator.dataset.carriedSecondNumber = ''
-                    calculator.dataset.previousKeyType = ''
-                } else {
-                    key.textContent = 'AC';
-                }
+                //if (key.textContent === 'AC'){
+                    //calculator.dataset.firstNumber = ''
+                    //calculator.dataset.operation = ''
+                    //calculator.dataset.carriedSecondNumber = ''
+                    //calculator.dataset.previousKeyType = ''
+                //} else {
+                    //ey.textContent = 'AC';
+                //}
 
-                display.textContent = 0;                
-
-                //Set custom attribute
-                calculator.dataset.previousKeyType = 'clear';
-            };        
-
-            if(action === 'decimal'){
-                console.log(action);
+                //display.textContent = 0;                
 
                 //Set custom attribute
-                calculator.dataset.previousKey = 'decimal';
+                //calculator.dataset.previousKeyType = 'clear';
+            //};        
+
+            //if(action === 'decimal'){
+               // console.log(action);
+
+                //Set custom attribute
+                //calculator.dataset.previousKey = 'decimal';
 
                 //If a decimal is pressed it also needs to be added to the current display
                 //if(!displayedNumber.includes('.')){
@@ -227,8 +224,8 @@ document.addEventListener('DOMContentLoaded', function(){
                     //display.textContent = '0.';
                 //}
 
-                calculator.dataset.previousKeyType = 'decimal'
-            };                 
+                //calculator.dataset.previousKeyType = 'decimal'
+            //};                 
 
             //If the display number is 0, the number pressed needs to replace it            
             //Also if decimal/number is clicked after calc, display should be 0
@@ -247,13 +244,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
             //AC- all clear- clears everything and resets the calc to initial state
             //CE- clear entry - clears current entry. Keeps previous # in memory
-            if (action !== 'clear'){
-                const clearButton = calculator.querySelector('[data-action="clear"]');
+            //if (action !== 'clear'){
+                //const clearButton = calculator.querySelector('[data-action="clear"]');
                 //This works: https://gomakethings.com/javascript-selectors-in-html/
-                console.log(clearButton)
+                //console.log(clearButton)
 
-                clearButton.textContent = 'CE';
-            }
+                //clearButton.textContent = 'CE';
+            //}
             
 
                 
