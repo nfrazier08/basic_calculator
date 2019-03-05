c786be2f02451d6eb1461cc46e09d064d36ebd1a
neliza <nicolasaelf64@gmail.com>
2019-03-04 21:07:54 -0700

    returns were causing my functions not to fire as expected, removed white space in returns

--- script.js
+++ script.js
@@ -3,20 +3,18 @@ document.addEventListener('DOMContentLoaded', function(){
     const calculator = document.querySelector('.calculator');
     const keys = calculator.querySelector('.calculator_keys');   
     const display = calculator.querySelector('.display');  
-    const state = calculator.dataset     
+    //const state = calculator.dataset   
+    
+    //const action = key.dataset.action
     
     const calculate = (n1, operator, n2) => {
-        const firstInput = parseFloat(n1);
-        const secondInput = parseFloat(n2);
+        const firstInput = parseFloat(n1)
+        const secondInput = parseFloat(n2)
 
-        if(operator === 'add')
-            return firstInput + secondInput
-        if(operator === 'subtract')
-            return firstInput - secondInput
-        if(operator === 'multiply')
-            return firstInput * secondInput
-        if(operator === 'divide')
-            return firstInput / secondInput        
+        if(operator === 'add') return firstInput + secondInput
+        if(operator === 'subtract') return firstInput - secondInput
+        if(operator === 'multiply') return firstInput * secondInput
+        if(operator === 'divide') return firstInput / secondInput        
     } //End of calculate function
 
     const getKeyType = (key) => {
@@ -32,21 +30,25 @@ document.addEventListener('DOMContentLoaded', function(){
     } //End of getKeyType function
 
     const createResultsString = (key, displayedNumber, state) => {
-        //VARIABLES
-        console.log("these are variables")
-        const keyType = getKeyType(key)
+        
+        const keyTextContent = key.textContent     
 
-        const keyTextContent = key.textContent          
-        //const action = key.dataset.action       // I don't think we need it because we have created the getKeyType function above 
-        const firstNumber = state.firstNumber   
-        const modifiedValue = state.modifiedValue //I believe this gets used below
-        const operator = state.operator
-        const previousKeyType = state.previousKeyType   
+        const keyType = getKeyType(key)                 
+        const {
+           firstNumber,
+           operator, 
+           modifiedValue, 
+           previousKeyType
+        } = state
+                
+        //const firstNumber = state.firstNumber   
+        //const modifiedValue = state.modifiedValue //I believe this gets used below
+        //const operator = state.operator
+        //const previousKeyType = state.previousKeyType   
 
         //IF STATEMENTS
         if(keyType === 'number') {
-           return 
-                displayedNumber === '0' || 
+           return displayedNumber === '0' || 
                 previousKeyType === 'operator' || 
                 previousKeyType === 'calculate'
             ? keyTextContent   
@@ -54,52 +56,52 @@ document.addEventListener('DOMContentLoaded', function(){
         }  
         
         if(keyType === 'decimal'){
-            if(!displayedNumber.includes('.')) 
-                return displayedNumber + '.'
-            if (previousKeyType === 'operator' || previousKeyType === 'calculate')
-                return '0.'            
+            if(!displayedNumber.includes('.')) return displayedNumber + '.'
+            if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.'            
             return displayedNumber                    
         }
 
-        if(
-            keyType === 'operator'){
+        if(keyType === 'operator'){
             //const firstNumber = calculator.dataset.firstNumber
             //const operator = calculator.dataset.operator
             //const secondNumber = displayedNumber
-
-            if (
-                firstNumber &&
+        
+            return firstNumber &&
                 operator &&
                 previousKeyType !== 'operator' &&
-                previousKeyType !== 'calculate'
-            ){
-                return calculate(firstNumber, operator, displayedNumber)
-            } else {
-                return displayedNumber
-                }
+                previousKeyType !== 'calculate'            
+                ? calculate(firstNumber, operator, displayedNumber)
+                : displayedNumber
             }
         
-        if(keyType === 'clear') 
-            return 0
+        
+        if(keyType === 'clear') return 0
 
         if(keyType === 'calculate'){
-            const firstNumber = calculator.dataset.firstNumber
-            const operator = state.operator        
-            const modifiedValue = state.modifiedValue
+            //const firstNumber = calculator.dataset.firstNumber
+            //const operator = state.operator        
+            //const modifiedValue = state.modifiedValue
+            return firstNumber
 
-            if (firstNumber){
-                return previousKeyType === 'calculate'
-                    ? calculate(firstNumber, operator, modifiedValue)        
-                    : calculate(firstNumber, operator, displayedNumber)
-            } else {
-                return displayedNumber
-            }
+            ? previousKeyType === 'calculate'
+                ? calculate(displayedNumber, operator, modifiedValue)        
+                : calculate(firstNumber, operator, displayedNumber)
+            :  displayedNumber            
         }        
     } //End of createResultsString
     
     //============================================================
     const updateCalculatorState = (key, calculator, calculatedValue, displayedNumber) => {
+        //alert("yes!")
         const keyType = getKeyType(key)
+        
+        const {
+            firstNumber, 
+            operator, 
+            modifiedValue, 
+            previousKeyType
+        } = calculator.dataset
+
         calculator.dataset.previousKeyType = keyType
 
         //Variables
@@ -109,23 +111,11 @@ document.addEventListener('DOMContentLoaded', function(){
         // displayedNum
         // modifiedValue
 
-        //Released 'selected' state from all keys
-        Array.from(key.parentNode.children)
-            .forEach(k => k.classList.remove('selected'));
-
-        if (keyType === 'number'){
-          
-        } //number
-
-        if (keyType === 'decimal'){
-
-        } //decimal
-
         if (keyType === 'operator') {
-            key.classList.add("selected"); 
+            //key.classList.add("selected"); 
 
             calculator.dataset.operator = key.dataset.action
-            calculator.dataset.firstNumber = key.dataset.firstNumber &&
+            calculator.dataset.firstNumber = firstNumber &&
                 operator && 
                 previousKeyType !== 'operator' && 
                 previousKeyType !== 'calculate'                    
@@ -133,44 +123,54 @@ document.addEventListener('DOMContentLoaded', function(){
             : displayedNumber                    
         } //operator
 
-        if (keyType === 'clear'){                                           
-            if (key.textContent === 'AC'){
+        if (keyType === 'clear' && key.textContent === 'AC'){                                           
                 calculator.dataset.firstNumber = ''
                 calculator.dataset.operator = ''
                 calculator.dataset.modifiedValue = ''
-                calculator.dataset.previousKeyType = ''
-            } else {
-                key.textContent = 'AC';
-            }
+                calculator.dataset.previousKeyType = ''            
         } //clear
 
-        if (keyType !== 'clear'){
-            const clearButton = calculator.querySelector('[data-action=clear]') 
-            clearButton.textContent = 'CE'
-        } //not clear
+        //if (keyType !== 'clear'){
+            //const clearButton = calculator.querySelector('[data-action=clear]') 
+            //clearButton.textContent = 'CE'
+        //} //not clear
 
         if(keyType === 'calculate'){
-            secondNumber = calculator.dataset.modifiedValue = firstNumber && previousKeyType === 'calculate'
+            calculator.dataset.modifiedValue = firstNumber && previousKeyType === 'calculate'
                 ? modifiedValue
                 : displayedNumber
         }          
         
     } //End of updateCalculatorState function
+
+    const updateVisualState = (key, calculator) => {
+        const keyType = getKeyType(key)
+
+        //Released 'selected' state from all keys
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('selected'));
+
+        if(keyType === 'operator') key.classList.add('selected')
+        if(keyType === 'clear' && key.textContent !== 'AC') key.textContent = 'AC'
+        if(keyType !== 'clear'){
+            const clearButton = calculator.querySelector('[data-action=clear]')
+            clearButton.textContent = 'CE'
+        }
+    }
     
     keys.addEventListener('click', e => {
-        //console.log("Is this where we are")
-        if (e.target.matches('button')) return
-
+        
+        if (!e.target.matches('button')) return        
 
             const key = e.target
-            const displayedNumber = display.textContent
+            const displayedNumber = display.textContent;
 
             //Pure Stuff       
             const resultString = createResultsString(key, displayedNumber, calculator.dataset)
-
+            
             //UpdateState
             display.textContent = resultString            
-            updateCalculatorState(key, calculator, resultString, displayedNumber)        
+            updateCalculatorState(key, calculator, resultString, displayedNumber)   
+            updateVisualState(key, calculator)     
     }) //End of click event
 
 
